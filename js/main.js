9986673b8556d2897017b53d1f7f357effb55d1c
Wes Hediger <wes.hediger@gmail.com>
2016-05-11 00:02:32 -0600

    create function to return selected catagory value

--- js/main.js
+++ js/main.js
@@ -4,16 +4,23 @@ $(document).ready(function(){
     var itemLength = data.list.item.length;
     for ( i = 0; i < itemLength; i++ ) {
         console.log(data.list.item[i].name);
+        $('#selectCatagory').append('<option'
+          + ' value="' + data.list.item[i].name
+          + '">' + data.list.item[i].name
+          + '</option>')
     }
+    return data;
+  }).then(function(data){
+    $('#selectCatagory').on('change', function(){
+      console.log($('#selectCatagory').val());
+      $('main').append('<p></p>');
+      $('main p:last-child').text($('#selectCatagory').val());
+    });
 
+    $('#submit').on('click', function(event){
+      event.preventDefault();
 
-    $('main').append('<p></p>');
-    $('p').text(data.list.item[0]);
-  });
-//------make above an iife +==}=======>
-  $('#submit').on('click', function(event){
-    event.preventDefault();
-
+    });
   });
   // need to get the item id number and then pass it back over to get nutritional information. Two seperate requests.
 });
