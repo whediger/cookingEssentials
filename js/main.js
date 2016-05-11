a0658d350fec38439b6e396879ec2c8da27a7fe4
Wes Hediger <wes.hediger@gmail.com>
2016-05-10 21:13:04 -0600

    add get function with test url

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  
+
+  $('#submit').on('click', function(event){
+    event.preventDefault();
+    console.log('stufffffffffffff');
+    $.get('http://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=DEMO_KEY ', function(data){
+      console.log(data);
+      $('main').append('<p></p>');
+      $('p').text(data);
+    });
+  });
+
 });
